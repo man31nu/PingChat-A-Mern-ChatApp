10070490784b3bf97b2f31c9bfbc0e1da525305b
man31nu <mannuzadav62015@gmail.com>
2026-03-31 01:21:12 +0530

    fix: updated JWT cookie sameSite policy to allow cross-site requests between Vercel and Render

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,9 +7,9 @@ const generateTokenAndSetCookie = (userId, res) => {
 
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",
+		httpOnly: true, // prevent XSS attacks
+		sameSite: process.env.NODE_ENV !== "development" ? "none" : "strict", // required for cross-site Vercel to Render requests
+		secure: process.env.NODE_ENV !== "development", // required for cross-site cookies
 	});
 };
 
