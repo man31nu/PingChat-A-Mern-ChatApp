9113862baabe6ecfc6e4d41b38c3915f59617c2e
man31nu <mannuzadav62015@gmail.com>
2026-03-31 00:26:17 +0530

    Auto set VITE_API_URL based on env to bypass vercel configuration

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://pingchat-a-mern-chatapp.onrender.com";
 export default BASE_URL;
